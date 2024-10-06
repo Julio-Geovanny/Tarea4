1413e2378f73ac82e21c593e01f1b0d99daa869c
Julio-Geovanny <geovannychg14@gmail.com>
2024-10-06 14:31:42 -0600

    Agregar archivos de proyecto.

---
+++ Tarea4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tarea4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
+// Direccion de carpeta: C:\Users\Geovanny\source\repos
